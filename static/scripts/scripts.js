e3b0f8f5112c334e5b2d7b7ece40467e67d5c286
Rafael Papallas <rpapallas@gmail.com>
2022-12-17 21:25:17 +0000

    Added license to files

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,10 +1,32 @@
+/* MIT License
+
+Copyright (c) 2022 Rafael Papallas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
 function showVideosAndHideWarning() {
-   var videos = document.getElementById("videos-block");
-   var warning = document.getElementById("warning-block");
+   const videos = document.getElementById("videos-block");
+   const warning = document.getElementById("warning-block");
 
    warning.style.display = "none";
 
-   var iframes = document.getElementsByTagName("iframe");
+   const iframes = document.getElementsByTagName("iframe");
    for(var i = 0; i < iframes.length; i++){
       iframes[i].setAttribute("src", iframes[i].getAttribute("data-src"));
    }
